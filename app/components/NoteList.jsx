1348942ea29a0aa0a3da8596cc5b64e71773438b
pixelshot <pixelshot8@gmail.com>
2022-12-15 17:40:40 +0800

    Same size across all boxes and is now mobile responsive

--- app/components/NoteList.jsx
+++ app/components/NoteList.jsx
@@ -1,18 +1,17 @@
 function NoteList({ notes }) {
   const { data } = notes;
-  console.log(data);
   return (
-    <ul className="container py-8 mx-auto gap-4 grid grid-cols-4 auto-rows-max">
+    <ul className="container py-8 mx-auto gap-4 grid md:grid-cols-4 overflow-auto">
       {data.map((result) => (
-        <li key={result.id} className="mx-auto mx-w-sm text-white">
+        <li key={result.id} className="mx-auto text-white">
           <a
             href="#"
-            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gblue dark:border-gdarkblue dark:hover:bg-gorange"
+            className="block p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gblue dark:border-gdarkblue dark:hover:bg-gorange"
           >
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               {result.title}
             </h5>
-            <p className="font-normal text-white dark:text-gray-400">
+            <p className="font-normal text-white dark:text-gray-400 h-24 overflow-auto">
               {result.content}
             </p>
             <time>
